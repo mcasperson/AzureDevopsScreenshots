5067679774f677a61ae27b73615c1abeb24dff95
Matthew Casperson <matthewcasperson@gmail.com>
2019-11-04 16:22:01 +1000

    Update azure.tf

--- azure.tf
+++ azure.tf
@@ -91,7 +91,7 @@ resource "azurerm_virtual_machine_extension" "test" {
 
   settings = <<SETTINGS
     {
-        "commandToExecute": "powershell.exe -Command \"Set-ExecutionPolicy Bypass -Scope Process -Force; Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')); choco install -y git; & 'C:\\Program Files\\Git\\bin\\git' clone https://github.com/OctopusDeploy/OctopusGuides.git; cd OctopusGuides; ./install.ps1 -Scripts azuredevops.pp,utilities.pp,azurewait.pp; & 'C:\\Program Files\\OpenJDK\\jdk-13\\bin\\java' --enable-preview '-Dwebdriver.gecko.driver=C:\\tools\\geckodriver.exe' '-DslackHookUrl=#{SlackWebHook}' '-DslackStepHandlerEnabled=true' -jar c:\\tools\\webdrivertraining-1.0-SNAPSHOT.jar --tags '(@login or @install-extensions)' features\\azuredevops\\azuredevops-aspnet-project.feature\""
+        "commandToExecute": "powershell.exe -Command \"Set-ExecutionPolicy Bypass -Scope Process -Force; Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')); choco install -y git; cd c:\; & 'C:\\Program Files\\Git\\bin\\git' clone https://github.com/OctopusDeploy/OctopusGuides.git; cd OctopusGuides; ./install.ps1 -Scripts azuredevops.pp,utilities.pp,azurewait.pp; & 'C:\\Program Files\\OpenJDK\\jdk-13\\bin\\java' --enable-preview '-Dwebdriver.gecko.driver=C:\\tools\\geckodriver.exe' '-DslackHookUrl=#{SlackWebHook}' '-DslackStepHandlerEnabled=true' -jar c:\\tools\\webdrivertraining-1.0-SNAPSHOT.jar --tags '(@login or @install-extensions)' features\\azuredevops\\azuredevops-aspnet-project.feature\""
     }
 SETTINGS
 }
